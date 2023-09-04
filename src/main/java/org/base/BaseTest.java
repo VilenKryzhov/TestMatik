b4e228365465d5cabb7d53796ac5e415544fd92a
VilenKryzhov <krijov.vilen@gmail.com>
2023-09-04 12:53:05 +0300

    Update test

--- src/main/java/org/base/BaseTest.java
+++ src/main/java/org/base/BaseTest.java
@@ -9,35 +9,32 @@ import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
-        @BeforeClass(alwaysRun = true, description = "create config for  test")// незалежно від того чи впав тест чи ні,  буде виконуватись анотація
-        public void selenideConfig() {
-            Configuration.holdBrowserOpen = true;
-            Configuration.startMaximized = true;
-            //Configuration.browserSize = "1920x1080";
-            Configuration.headless = false;
-            Configuration.screenshots = true;
-            Configuration.pageLoadStrategy = "normal";
-            Configuration.pageLoadTimeout = 20000;
-            Configuration.timeout = 10000;
-            Configuration.reportsFolder = "screenshots";
-        }
-
-        @BeforeMethod(alwaysRun = true, description = "open basic URL")
-        public void setUp() {
-            Selenide.open("https://www.amazon.com/ ");
-        }
-
-
-        @AfterMethod(alwaysRun = true, description = "WebDriver clean up")
-        public void cleanWebDriver() {
-            Selenide.clearBrowserCookies();
-//            Selenide.refresh();
-//        Selenide.open("about:blank");
-        }
-
-       @AfterSuite(alwaysRun = true, description = "Closing browser")
-        public void tearDown() {
-            Selenide.closeWebDriver();
-        }
+    @BeforeClass(alwaysRun = true, description = "create config for  test")
+    public void selenideConfig() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.startMaximized = true;
+        Configuration.headless = false;
+        Configuration.screenshots = true;
+        Configuration.pageLoadStrategy = "normal";
+        Configuration.pageLoadTimeout = 20000;
+        Configuration.timeout = 10000;
+        Configuration.reportsFolder = "screenshots";
     }
+
+    @BeforeMethod(alwaysRun = true, description = "open basic URL")
+    public void setUp() {
+        Selenide.open("https://www.amazon.com/ ");
+    }
+
+
+    @AfterMethod(alwaysRun = true, description = "WebDriver clean up")
+    public void cleanWebDriver() {
+        Selenide.clearBrowserCookies();
+    }
+
+    @AfterSuite(alwaysRun = true, description = "Closing browser")
+    public void tearDown() {
+        Selenide.closeWebDriver();
+    }
+}
 
---
+++ src/test/java/AmazonSearchTest.java
@@ -0,0 +1,57 @@
+import com.codeborne.selenide.Selenide;
+import org.base.AmazonBooksPage;
+import org.base.BaseTest;
+import org.base.Books;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AmazonSearchTest extends BaseTest {
+    String url = "https://www.amazon.com/ ";
+    AmazonBooksPage amazonBooksPage = new AmazonBooksPage();
+    List<Books> booksList = new ArrayList<>();
+    List<String> filterAuthorList = new ArrayList<>();
+
+    @Test
+    public void test() {
+        amazonBooksPage.openAmazonHomePage(url);
+
+        amazonBooksPage.clickFilterBtn();
+
+        amazonBooksPage.clickBooksFilter();
+
+        amazonBooksPage.typeValueInInput("Java");
+
+        List<String> titleList = amazonBooksPage.getTitleList();
+        List<String> authorList = amazonBooksPage.getAuthorList();
+
+        amazonBooksPage.getFilterAuthorList(authorList, filterAuthorList);
+
+        List<String> priceList = amazonBooksPage.getPriceList();
+
+
+        Selenide.sleep(1000);
+
+
+        for (int i = 0; i < titleList.size(); i++) {
+            booksList.add(new Books(titleList.get(i), filterAuthorList.get(i)
+                    , priceList.get(i)));
+        }
+
+        String nameOfBook = null;
+
+        for (Books book : booksList) {
+            if (book.getTitle().equals("Head First Java, 2nd Edition")) {
+                nameOfBook = book.getTitle();
+            }
+            System.out.println("Book Name:    " + book.getTitle());
+            System.out.println("Book author:  " + book.getAuthor());
+            System.out.println("Book price:   $" + book.getPrice());
+            System.out.println(" ");
+        }
+
+        Assert.assertEquals(nameOfBook, "Head First Java, 2nd Edition");
+    }
+}
