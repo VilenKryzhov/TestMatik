a1b44bd7783c265bc41177ff21c7380622420f30
VilenKryzhov <krijov.vilen@gmail.com>
2023-09-13 17:16:21 +0300

    cmt

--- src/main/java/org/pages/HomePage.java
+++ src/main/java/org/pages/HomePage.java
@@ -48,33 +48,6 @@ public class HomePage {
         return priceList.texts();
     }
 
-    public void createBooksList(List<String> titleList, List<String> authorList, List<String> priceList) {
-
-        for (int i = 0; i < titleList.size(); i++) {
-            booksList.add(new Books(titleList.get(i), authorList.get(i)
-                    , priceList.get(i)));
-        }
-    }
-
-    public String getNameOfBook(){
-        for (Books book : booksList) {
-            if (book.getTitle().equals("Head First Java, 2nd Edition")) {
-                nameOfBook = book.getTitle();
-            }
-        }
-            return nameOfBook;
-    }
-
-    public void getFilterAuthorList(List<String> list, List<String> filterList) {
-        for (String s : list) {
-            if (s.contains("by")) {
-                int elementBy = s.indexOf("By");
-                int elementBy2 = s.indexOf("|");
-                String substring = s.substring(elementBy + 1, elementBy2 + 1);
-                filterList.add(substring);
-            }
-        }
-    }
 
 
 }
